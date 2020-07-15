848d8bbadaf107785d32d5e67cb74546b59082cc
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-07-15 15:55:59 +0200

    Update tilelib.js

--- scripts/tilelib.js
+++ scripts/tilelib.js
@@ -11,6 +11,14 @@ module.exports = {
   pointingAt(tileCheck, tile){
 	  return this.pointAt(tileCheck.x, tileCheck.y, tileCheck.rotation(), tile.x, tile.y);
   },
+  pointSide(tileCheck, tile){
+	  //return !((tileCheck.rotation()-tile.rotation())&1) && (abs(tileCheck.x-tile.x)==1?!abs(tileCheck.y-tile.y)==1:abs(tileCheck.y-tile.y)==1);
+	  return (abs(tileCheck.x-tile.x)==1 && tileCheck.rotation()&1)||(abs(tileCheck.y-tile.y)==1 && !(tileCheck.rotation()&1));
+  },
+  pointBack(tileCheck, tile){
+	  //return abs(tileCheck.rotation()-tile.rotation())==2 && (abs(tileCheck.x-tile.x)==1?!abs(tileCheck.y-tile.y)==1:abs(tileCheck.y-tile.y)==1);
+	  return !this.pointingAt(tileCheck, tile) && !pointSide(tileCheck, tile);
+  },
   isMod(tile){
 	  return tile.block().name.startsWith("bytmod");
   },
