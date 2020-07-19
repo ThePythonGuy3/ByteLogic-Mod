0f51802f226f21a05dff026ed4dbdfcd43e59ce1
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-07-19 10:09:00 +0200

    Eldoofus, are you happy now?
    Update signalrouter.js

--- scripts/signalrouter.js
+++ scripts/signalrouter.js
@@ -1,53 +1,33 @@
-const timerid = 0;
 const tilel = require("tilelib");
 const signalrouter = extendContent(Block, "signalrouter", {
-	  update(tile){
-	  entity = tile.ent();
-    tile.ent().timer.reset(timerid, 0);
-    //calculate additional stuff here with tile.ent().getSignal();
-    try{
-      //you need sth better than a try here; check if the next entity has the setSignal method.
-	  var[4] idklol = [tile.right(), tile.back(), tile.left(), tile.front()];
-	  for(int i=0;i<4;i++){
-		  if(idklol[i].block().name=="bytmod-relay" && !pointingAt(idklol[i], tile)) idklol[i].ent().setSignal(entity.getSignal());
-		  else if(idklol[i].block().name=="bytmod-display") idklol[i].ent().setSignal(entity.getSignal());
-		  else if(idklol[i].block().name=="bytmod-signalrouter" && /*check if this router is not source to prevent infinite signal passback*/) idklol[i].ent().setSignal(entity.getSignal());
-		  else if(idklol[i].block().name=="bytmod-signalfont"||idklol[i].block().name=="bytmod-switch"||idklol[i].block().name=="bytmod-analyzer") continue;
-		  else if((idklol[i].block().name=="bytmod-not"||idklol[i].block().name=="bytmod-decimalconv"||idklol[i].block().name=="bytmod-binaryconv") && pointBack(idklol[i], tile)) idklol[i].ent().setSignal(entity.getSignal());
-		  else if(pointSide(idklol[i], tile)) idklol[i].ent().setSignal(entity.getSignal());
-		  else continue;
-	  }
-    }catch(err){}
-    tile.ent().internalSignal(tile.ent().getTempSignal());
-    tile.ent().setTempSignal(0);
-    	if(entity.getSignal() == NaN){
-    		entity.internalSignal(0);
-    	}
+	update(tile){
+		entity = tile.ent();
+
+    	if(tilel.isMod(tile.front()) && tile.front().ent().asignal() == true && !tilel.pointingAt(tile.front(), tile)){
+            tile.front().ent().setSignal(entity.getSignal());
+        }
+	if(tilel.isMod(tile.back()) && tile.back().ent().asignal() == true && !tilel.pointingAt(tile.back(), tile)){
+            tile.back().ent().setSignal(entity.getSignal());
+        }
+if(tilel.isMod(tile.right()) && tile.right().ent().asignal() == true && !tilel.pointingAt(tile.right(), tile)){
+            tile.right().ent().setSignal(entity.getSignal());
+        }
+if(tilel.isMod(tile.left()) && tile.left().ent().asignal() == true && !tilel.pointingAt(tile.left(), tile)){
+            tile.left().ent().setSignal(entity.getSignal());
+        }
 	},
-  generateIcons(){
-    return[
-      Core.atlas.find("bytmod-logic-base"),
-      Core.atlas.find(this.name)
-    ]
-  },
+	generateIcons(){
+		return[
+			Core.atlas.find("bytmod-logic-base"),
+			Core.atlas.find(this.name)
+		]
+	},
 	draw(tile){
 		entity = tile.ent();
 		Draw.rect(Core.atlas.find("bytmod-logic-base"), tile.drawx(), tile.drawy());
 		Draw.color(entity.getSignal() > 0 ? Pal.accent : Color.white);
 		Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy(), tile.rotation()*90);
-  		if(tile.getNearby(0).block().name.startsWith("bytmod") && tile.getNearby(0).rotation()*90 == 180){
-			Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy(), 0);
-		}
-		if(tile.getNearby(2).block().name.startsWith("bytmod") && tile.getNearby(2).rotation()*90 == 0){
-			Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy(), 180);
-		}
-		if(tile.getNearby(1).block().name.startsWith("bytmod") && tile.getNearby(1).rotation()*90 == 270){
-			Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy(), 90);
-		}
-		if(tile.getNearby(3).block().name.startsWith("bytmod") && tile.getNearby(3).rotation()*90 == 90){
-			Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy(), 270);
-		}
-		Draw.reset();
+  		Draw.reset();
   	},
   	setBars(){
   		this.super$setBars();
@@ -61,38 +41,20 @@ const signalrouter = extendContent(Block, "signalrouter", {
 		}));
   	}
 });
-signalrouter.category = Category.power;
-signalrouter.size = 1;
-signalrouter.entityType = prov(() => {
+adder.category = Category.power;
+adder.size = 1;
+adder.entityType = prov(() => {
 	const entity = extend(TileEntity, {
 		getSignal: function(){
 			return this._signal;
 		},
 		setSignal: function(val){
-      		//check if it should take signal(ex. adders should not accept back) here.
-      		if(this.timer.getTime(timerid)==0){
-        		this._signal += val;//relays are "add mode"
-      		} else {
-        		this._tsignal += val;
-      		}
-    	},
-		getTempSignal: function(){
-			return this._tsignal;
-		},
-		setTempSignal: function(val){
-			this._tsignal = val;
-		},
-		addTempSignal: function(val){
-			this._tsignal += val;
-		},
-		internalSignal: function(val){
-			this._signal = val;//do not call in other blocks!
+			this._signal = val;
 		},
 		asignal: function(){
 			return true;
 		}
 	});
-	entity.internalSignal(0);
-	entity.setTempSignal(0);
+	entity.setSignal(0);
 	return entity;
 });
