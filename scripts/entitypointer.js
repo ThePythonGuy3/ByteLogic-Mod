ec6a662c1bb205bb8a9ad280eccf8893674017f1
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-14 09:13:44 +0200

    h

--- scripts/entitypointer.js
+++ scripts/entitypointer.js
@@ -1,8 +1,22 @@
 const tilel = require("tilelib");
+function closest(x, y, width, height, predicate) {
+        result = null;
+        cdist = 0.0;
+        Units.nearby(x, y, width, height, cons(e => {
+            if (predicate.get(e)) {
+                dist = Mathf.dst2(e.x, e.y, x, y);
+                if (result == null || dist < cdist) {
+                    result = e;
+                    cdist = dist;
+                }
+            }
+        }));
+        return result;
+}
 const entitypointer = extendContent(Block, "entitypointer", {
 	update(tile){
 		entity = tile.ent();
-    	target = UnitsUtil.closest(tile.worldx(), tile.worldy(), Vars.tilesize * (10 + size / 2), boolp(() => true));
+    	target = closest(tile.worldx(), tile.worldy(), Vars.tilesize * (10 + this.size / 2), boolp(e => !e.isDead));
     	if(target != null){
     		entity.setSignal(Angles.angle(tile.drawx(), tile.drawy(), target.getX(), target.getY()));
     	} else {
